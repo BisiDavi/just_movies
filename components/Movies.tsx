8b4d01ad67a4c9ec5d3eefe60fc18e2b5c8f4eae
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 04:13:30 +0100

    Homepage updated

--- components/Movies.tsx
+++ components/Movies.tsx
@@ -1,6 +1,8 @@
-import Box from "@mui/material/Box";
+import { Grid, Typography } from "@mui/material";
+import Container from "@mui/material/Container";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Image from "next/image";
 
 function fetchMovies() {
   return axios.get(
@@ -8,11 +10,42 @@ function fetchMovies() {
   );
 }
 
-export default function Movies({ movies }: any) {
+export default function Movies() {
   const { data, status } = useQuery({
     queryKey: ["movies"],
     queryFn: fetchMovies,
   });
   console.log("data", data);
-  return <Box></Box>;
+  return (
+    <Container sx={{ my: 4 }}>
+      {status === "error" ? (
+        <Typography>Unable to fetch movies</Typography>
+      ) : status === "loading" ? (
+        <Typography>Fetching...</Typography>
+      ) : (
+        <Grid container sx={{ margin: "auto", display: "flex" }}>
+          {data.data.Search.map((movie) => (
+            <Grid
+              key={movie.imdbId}
+              xs={12}
+              md={3}
+              sx={{ my: 2, mx: 0, display: "flex", flexDirection: "column" }}
+            >
+              <Image
+                src={movie.Poster}
+                alt={movie.Title}
+                height={200}
+                width={250}
+                blurDataURL={movie.Poster}
+                placeholder="blur"
+              />
+              <Typography sx={{ fontWeight: 600, textAlign: "center", my: 1 }}>
+                {movie.Title}
+              </Typography>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Container>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import Movies from "@/components/Movies";
 import Layout from "@/layout";
-import ProviderLayout from "@/layout/ProviderLayout";
 
 export default function Home() {
   return (
     <Layout>
-      <ProviderLayout>
-        <Movies />
-      </ProviderLayout>
+      <Movies />
     </Layout>
   );
 }
