1fc460378234985e8948bfc84e6b56f609acdadd
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 18:39:55 +0100

    ProviderLayout updated

--- layout/ProviderLayout.tsx
+++ layout/ProviderLayout.tsx
@@ -1,10 +1,14 @@
+import store from "@/redux/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
 
-export default function ProviderLayout({children}:PropsWithChildren) {
+export default function ProviderLayout({ children }: PropsWithChildren) {
   const queryClient: any = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </Provider>
   );
 }
