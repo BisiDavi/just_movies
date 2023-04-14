0865fa56266e0bde34eac6f9dc95f1c550908b75
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 13:46:41 +0100

    pages updated

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -34,16 +34,44 @@ export default function MoviesPage() {
               <Image
                 src={data?.data.Poster}
                 alt={data?.data.Title}
-                height={600}
-                width={600}
+                height={500}
+                width={500}
                 placeholder="blur"
                 blurDataURL={data?.data.Poster}
               />
             </Grid>
             <Grid item xs={12} md={6}>
-              <Typography variant="h4" sx={{ textAlign: "center" }}>
-                {data?.data.Title}
+              <Typography variant="h4" sx={{ textAlign: "center", mb: 2 }}>
+                {data?.data.Title} -{" "}
+                <span style={{ fontSize: "20px" }}>{data?.data.Year}</span>
               </Typography>
+              <Typography>Plot: {data?.data.Plot}</Typography>
+              <Typography sx={{ mt: 3 }}>
+                Actors: {data?.data.Actors}
+              </Typography>
+              <Typography>Director: {data?.data.Director}</Typography>
+              <Typography>Writer: {data?.data.Writer}</Typography>
+              <Typography>Genre: {data?.data.Genre}</Typography>
+              <Typography>Language: {data?.data.Language}</Typography>
+              <Typography>Country: {data?.data.Country}</Typography>
+              <Typography>Awards: {data?.data.Awards}</Typography>
+              <Typography>Awards {data?.data.Awards}</Typography>
+              <Typography>Released: {data?.data.Released}</Typography>
+              <Typography>Rated: {data?.data.Rated}</Typography>
+              <Typography>Runtime: {data?.data.Runtime}</Typography>
+              <Typography>IMDb Rating: {data?.data.imdbRating}</Typography>
+              <Typography>IMDb Votes: {data?.data.imdbVotes}</Typography>
+              {data?.data.BoxOffice && (
+                <Typography>BoxOffice: {data?.data.BoxOffice}</Typography>
+              )}
+              <Box sx={{ mt: 2 }}>
+                <Typography>Ratings</Typography>
+                {data?.data.Ratings.map((rating) => (
+                  <Typography key={rating.Source}>
+                    {rating.Source}: {rating.Value}
+                  </Typography>
+                ))}
+              </Box>
             </Grid>
           </Grid>
         )}
