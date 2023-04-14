2ccc815ebee576cdce3d2b6142a77310f5971873
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 14:19:15 +0100

    movie components updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     <>
       <Head>
         <title>Mail Movies</title>
-        <meta name="description" content="Book Movies in minutes" />
+        <meta name="description" content="Get full details about your favourite movie via Mail Movies straight to your e-mail" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
