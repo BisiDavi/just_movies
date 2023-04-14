9a61e7bb89af4859b2faad865c60512e04a9a9d1
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 20:08:06 +0100

    MovieDetailTextView added

--- components/MovieDetailTextView.tsx
+++ components/MovieDetailTextView.tsx
@@ -1,8 +1,11 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Input from "@mui/material/Input";
+import InputAdornment from "@mui/material/InputAdornment";
+import MailIcon from "@mui/icons-material/Mail";
 import type { movieDetailType } from "@/types";
-
 interface Props {
   movie: movieDetailType;
 }
@@ -51,6 +54,59 @@ export default function MovieDetailTextView({ movie }: Props) {
             {rating.Source}: {rating.Value}
           </Typography>
         ))}
+      </Box>
+      <Box sx={{ mt: 10 }}>
+        <Typography sx={{ fontWeight: "bold", mb: 2 }}>
+          Drop your email address to receive full gist about {movie.Title}
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            position: "relative",
+            backgroundColor: "red",
+            p: "5px 10px",
+            borderRadius: "4px",
+            width: "80%",
+          }}
+        >
+          <Input
+            placeholder="Email Address"
+            sx={{
+              p: "1px 40px",
+              backgroundColor: "white",
+              width: "100%",
+              borderTopLeftRadius: "4px",
+              borderBottomLeftRadius: "4px",
+            }}
+            type="email"
+            required
+          />
+          <InputAdornment
+            position="start"
+            sx={{ position: "absolute", left: 18 }}
+          >
+            <MailIcon />
+          </InputAdornment>
+          <Button
+            sx={{
+              backgroundColor: "black",
+              color: "white",
+              borderRadius: 0,
+              borderTopRightRadius: 5,
+              borderBottomRightRadius: 5,
+              fontSize: "12px",
+              "&:hover": {
+                backgroundColor: "black",
+                color: "white",
+                opacity: 0.5,
+              },
+            }}
+          >
+            Submit
+          </Button>
+        </Box>
       </Box>
     </Grid>
   );
