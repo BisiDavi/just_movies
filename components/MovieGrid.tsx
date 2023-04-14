4889557da3b947eac4afe2e6cde28025f872b05b
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 12:38:18 +0100

    Movie page updated

--- components/MovieGrid.tsx
+++ components/MovieGrid.tsx
@@ -2,23 +2,26 @@ import Grid from "@mui/material/Grid";
 
 import MovieCard from "@/components/MovieCard";
 import { MovieType } from "@/types";
+import Link from "next/link";
 
 export default function MovieGrid({ data }: any) {
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fit,minmax(280px,1fr))",
-        rowGap: "10px",
-        m: 0,
-        width: "100%",
-      }}
-    >
-      {data.data.Search.map((movie: MovieType) => (
-        <MovieCard key={`${movie.imdbId}-${movie.Title}`} movie={movie} />
-      ))}
-    </Grid>
+    <Link href="/movies">
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit,minmax(280px,1fr))",
+          rowGap: "10px",
+          m: 0,
+          width: "100%",
+        }}
+      >
+        {data.data.Search.map((movie: MovieType) => (
+          <MovieCard key={`${movie.imdbId}-${movie.Title}`} movie={movie} />
+        ))}
+      </Grid>
+    </Link>
   );
 }
