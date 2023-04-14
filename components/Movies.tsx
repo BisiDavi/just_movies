812c2c883f0ffe7f53a86410643aecc399227202
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 03:08:36 +0100

    Movies added

--- components/Movies.tsx
+++ components/Movies.tsx
@@ -1,9 +1,14 @@
 import Box from "@mui/material/Box";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-export default function Movies({movies}:any) {
-  return (
-    <Box>
+function fetchMovies() {
+  axios.get(
+    `http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_MOVIE_API}s=home&plot=short`
+  );
+}
 
-    </Box>
-  )
+export default function Movies({ movies }: any) {
+    const {} = useQuery({})
+  return <Box></Box>;
 }
