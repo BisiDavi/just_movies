335a1998ebf8da1027d4cc3d2cf734ce3dd0585e
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 03:43:34 +0100

    header updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 import Logo from "@/components/Logo";
 import Search from "@/components/Search";
+import { Container } from "@mui/material";
 
 export default function Header() {
   return (
@@ -13,10 +14,23 @@ export default function Header() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box component="header">
-        <Logo />
-
-        <Search />
+      <Box
+        component="header"
+        sx={{
+          bgcolor: "red",
+          color: "white",
+        }}
+      >
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            p: 2
+          }}
+        >
+          <Logo />
+          <Search />
+        </Container>
       </Box>
     </>
   );
