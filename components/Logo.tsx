5edcbbf33b7530d0fe14521c5ef660f44d3f273b
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 02:49:57 +0100

    logo added

---
+++ components/Logo.tsx
@@ -0,0 +1,12 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+export default function Logo() {
+  return (
+    <Box>
+      <Typography>
+        Book<span>Movies</span>
+      </Typography>
+    </Box>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import Logo from "@/components/Logo";
 import Layout from "@/layout";
 
 
 export default function Home() {
-  return <Layout></Layout>;
+  return <Layout>
+    <Logo />
+  </Layout>;
 }
