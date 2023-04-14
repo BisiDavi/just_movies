9a62b12d6f7608907369276fb64516ba06bc853f
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 05:38:10 +0100

    logo updated

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,15 +1,18 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Box sx={{ border: "1px solid white", py: 1, px: 2 }}>
-      <Typography sx={{ fontWeight: "bold" }}>
-        Book
-        <Box sx={{ color: "black" }} component="span">
-          Movies
-        </Box>
-      </Typography>
-    </Box>
+    <Link href="/" style={{ textDecoration: "none" }}>
+      <Box sx={{ border: "1px solid white", py: 1, px: 2 }}>
+        <Typography sx={{ fontWeight: "bold", color: "white" }}>
+          Book
+          <Box sx={{ color: "black" }} component="span">
+            Movies
+          </Box>
+        </Typography>
+      </Box>
+    </Link>
   );
 }
