56e8d00a75494605a25744ae46aef5af95325985
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 13:51:26 +0100

    MovieDetailView added

---
+++ components/MovieDetailView.tsx
@@ -0,0 +1,73 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Image from "next/image";
+
+interface Props {
+  movie: {
+    Poster: string;
+    Title: string;
+    Year: string;
+    Plot: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Language: string;
+    Awards: string;
+    Country: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Rated: string;
+    imdbRating: string;
+    imdbVotes: string;
+    BoxOffice: string;
+    Ratings: Array<{ Source: String; Value: string }>;
+  };
+}
+
+export default function MovieDetailView({ movie }: Props) {
+  return (
+    <Grid container>
+      <Grid item xs={12} md={6}>
+        <Image
+          src={movie.Poster}
+          alt={movie.Title}
+          height={500}
+          width={500}
+          placeholder="blur"
+          blurDataURL={movie.Poster}
+        />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Typography variant="h4" sx={{ textAlign: "center", mb: 2 }}>
+          {movie.Title} - <span style={{ fontSize: "20px" }}>{movie.Year}</span>
+        </Typography>
+        <Typography>Plot: {movie.Plot}</Typography>
+        <Typography sx={{ mt: 3 }}>Actors: {movie.Actors}</Typography>
+        <Typography>Director: {movie.Director}</Typography>
+        <Typography>Writer: {movie.Writer}</Typography>
+        <Typography>Genre: {movie.Genre}</Typography>
+        <Typography>Language: {movie.Language}</Typography>
+        <Typography>Country: {movie.Country}</Typography>
+        <Typography>Awards {movie.Awards}</Typography>
+        <Typography>Released: {movie.Released}</Typography>
+        <Typography>Rated: {movie.Rated}</Typography>
+        <Typography>Runtime: {movie.Runtime}</Typography>
+        <Typography>IMDb Rating: {movie.imdbRating}</Typography>
+        <Typography>IMDb Votes: {movie.imdbVotes}</Typography>
+        {movie.BoxOffice && (
+          <Typography>BoxOffice: {movie.BoxOffice}</Typography>
+        )}
+        <Box sx={{ mt: 2 }}>
+          <Typography>Ratings</Typography>
+          {movie.Ratings.map((rating, index) => (
+            <Typography key={`${rating.Source}-${index}`}>
+              {rating.Source}: {rating.Value}
+            </Typography>
+          ))}
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
