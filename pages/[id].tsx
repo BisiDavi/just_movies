b37b5205cfc52395b1ce5e29463c58b4670f5a4a
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 13:52:29 +0100

    Movie page updated

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,14 +1,13 @@
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 
 import SpinnerRipple from "@/components/SpinnerRipple";
 import Layout from "@/layout";
 import { fetchMovie } from "@/utils/fetchMovies";
-import Image from "next/image";
+import MovieDetailView from "@/components/MovieDetailView";
 
 export default function MoviesPage() {
   const router = useRouter();
@@ -29,51 +28,7 @@ export default function MoviesPage() {
             <SpinnerRipple />
           </Box>
         ) : (
-          <Grid container>
-            <Grid item xs={12} md={6}>
-              <Image
-                src={data?.data.Poster}
-                alt={data?.data.Title}
-                height={500}
-                width={500}
-                placeholder="blur"
-                blurDataURL={data?.data.Poster}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography variant="h4" sx={{ textAlign: "center", mb: 2 }}>
-                {data?.data.Title} -{" "}
-                <span style={{ fontSize: "20px" }}>{data?.data.Year}</span>
-              </Typography>
-              <Typography>Plot: {data?.data.Plot}</Typography>
-              <Typography sx={{ mt: 3 }}>
-                Actors: {data?.data.Actors}
-              </Typography>
-              <Typography>Director: {data?.data.Director}</Typography>
-              <Typography>Writer: {data?.data.Writer}</Typography>
-              <Typography>Genre: {data?.data.Genre}</Typography>
-              <Typography>Language: {data?.data.Language}</Typography>
-              <Typography>Country: {data?.data.Country}</Typography>
-              <Typography>Awards: {data?.data.Awards}</Typography>
-              <Typography>Awards {data?.data.Awards}</Typography>
-              <Typography>Released: {data?.data.Released}</Typography>
-              <Typography>Rated: {data?.data.Rated}</Typography>
-              <Typography>Runtime: {data?.data.Runtime}</Typography>
-              <Typography>IMDb Rating: {data?.data.imdbRating}</Typography>
-              <Typography>IMDb Votes: {data?.data.imdbVotes}</Typography>
-              {data?.data.BoxOffice && (
-                <Typography>BoxOffice: {data?.data.BoxOffice}</Typography>
-              )}
-              <Box sx={{ mt: 2 }}>
-                <Typography>Ratings</Typography>
-                {data?.data.Ratings.map((rating) => (
-                  <Typography key={rating.Source}>
-                    {rating.Source}: {rating.Value}
-                  </Typography>
-                ))}
-              </Box>
-            </Grid>
-          </Grid>
+          <MovieDetailView movie={data.data} />
         )}
       </Container>
     </Layout>
