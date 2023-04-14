0204979533b2b168f9f30eddbaf7b3bd1b2a8a06
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 17:06:34 +0100

    search updated

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,16 @@
 import Input from "@mui/material/Input";
 import Box from "@mui/material/Box";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Search() {
   return (
-    <Box sx={{ color: "white", width: { md: "400px", xs: "100%" } }}>
+    <Box
+      sx={{
+        color: "white",
+        width: { md: "400px", xs: "100%", position: "relative" },
+      }}
+    >
       <Input
         placeholder="Search for movies"
         sx={{ color: "white", "::placeholder": { color: "white" } }}
@@ -11,6 +18,12 @@ export default function Search() {
         color="info"
         className="search_movies"
       />
+      <InputAdornment
+        position="end"
+        sx={{ position: "absolute", zIndex: 4, right: "10px", top: "16px" }}
+      >
+        <SearchIcon />
+      </InputAdornment>
     </Box>
   );
 }
