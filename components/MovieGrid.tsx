73fd7fe7875fa3b9e05209efa793ac404de91100
BisiDavi <oludavidconnect@gmail.com>
2023-04-15 04:42:54 +0100

    movieGrid updated

--- components/MovieGrid.tsx
+++ components/MovieGrid.tsx
@@ -1,8 +1,9 @@
+import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 import MovieCard from "@/components/MovieCard";
 import type { MovieType } from "@/types";
-import { Typography } from "@mui/material";
 
 interface Props {
   data: {
@@ -15,30 +16,35 @@ interface Props {
 
 export default function MovieGrid({ data }: Props) {
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr))",
-        gap: "30px",
-        m: 0,
-        width: "100%",
-        minHeight: "500px",
-      }}
-    >
-      {data.data.Response === "True" ? (
-        data.data.Search.map((movie: MovieType) => (
-          <MovieCard key={`${movie.imdbID}-${movie.Title}`} movie={movie} />
-        ))
-      ) : (
-        <Typography
-          variant="h4"
-          sx={{ textAlign: "center", fontWeight: "bold", m: "auto" }}
+    <>
+      {data.data.Response === "True" && (
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr))",
+            gap: "30px",
+            m: 0,
+            width: "100%",
+            minHeight: "500px",
+          }}
         >
-          No Result for this search.
-        </Typography>
+          {data.data.Search.map((movie: MovieType) => (
+            <MovieCard key={`${movie.imdbID}-${movie.Title}`} movie={movie} />
+          ))}
+        </Grid>
       )}
-    </Grid>
+      {data.data.Response !== "True" && (
+        <Box sx={{ minHeight: "66vh", display: "flex" }}>
+          <Typography
+            variant="h4"
+            sx={{ textAlign: "center", fontWeight: "bold", m: "auto" }}
+          >
+            No Result for this search.
+          </Typography>
+        </Box>
+      )}
+    </>
   );
 }
