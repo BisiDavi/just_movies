f6534d73ff1bc7c6da135e0fc76a4b9073dd66f8
BisiDavi <oludavidconnect@gmail.com>
2023-04-15 21:24:58 +0100

    toastcontainer added

--- layout/ProviderLayout.tsx
+++ layout/ProviderLayout.tsx
@@ -2,6 +2,7 @@ import store from "@/redux/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
 
 export default function ProviderLayout({ children }: PropsWithChildren) {
   const queryClient: any = new QueryClient();
@@ -9,6 +10,7 @@ export default function ProviderLayout({ children }: PropsWithChildren) {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <ToastContainer />
     </Provider>
   );
 }
