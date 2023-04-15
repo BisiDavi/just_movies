e69c84cb305d085622452dd6882d2be3c7a0464b
BisiDavi <oludavidconnect@gmail.com>
2023-04-15 15:03:10 +0100

    generateMovieLD added

---
+++ utils/generateMovieLD.ts
@@ -0,0 +1,44 @@
+import type { movieDetailType } from "@/types";
+
+type castType = Array<{ "@type": string; name: string }>;
+
+export default function generateMovieLD(movie: movieDetailType) {
+  const _actors = movie.Actors.split(",");
+  const _writer = movie.Writer.split(",");
+
+  let actors: castType = [];
+  _actors.map((actor) =>
+    actors.push({
+      "@type": "Person",
+      name: actor,
+    })
+  );
+
+  let writer: castType = [];
+  _writer.map((actor) =>
+    writer.push({
+      "@type": "Person",
+      name: actor,
+    })
+  );
+
+  return {
+    __html: `{
+          "@context": "https://schema.org",
+          "@type": ${movie.Genre},
+          "actor": ${actors},
+          "aggregateRating": {
+            "@type": "AggregateRating",
+            "bestRating": ${movie.imdbRating},
+            "ratingCount": ${movie.imdbVotes},
+          },
+          "author": ${writer},
+          "description": ${movie.Plot}
+          "director": {
+            "@type": "Person",
+            "name": ${movie.Director}
+          },
+          "name":${movie.Title}
+        }`,
+  };
+}
