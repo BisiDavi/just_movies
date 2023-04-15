faff8db31f46a0214bd21059ef273777bd5b28ba
BisiDavi <oludavidconnect@gmail.com>
2023-04-15 04:50:12 +0100

    send email api updated

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -5,42 +5,43 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, data } = req.body;
   switch (req.method) {
     case "POST": {
-      mailjet
-        .apiConnect(
-          `${process.env.MAILJET_API_KEY}`,
-          `${process.env.MAILJET_SECRET_KEY}`
-        )
-        .post("send", { version: "v3.1" })
-        .request({
-          Messages: [
-            {
-              From: {
-                Email: "oludavidconnect@gmail.com",
-                Name: "MailMovies",
-              },
-              To: [
-                {
-                  Email: email,
-                  Name: email,
+      try {
+        mailjet
+          .apiConnect(
+            `${process.env.MAILJET_API_KEY}`,
+            `${process.env.MAILJET_SECRET_KEY}`
+          )
+          .post("send", { version: "v3.1" })
+          .request({
+            Messages: [
+              {
+                From: {
+                  Email: "oludavidconnect@gmail.com",
+                  Name: "MailMovies",
                 },
-              ],
-              TemplateID: process.env.MAILJET_TEMPLATE_ID,
-              TemplateLanguage: true,
-              Subject: `Details about ${data.Title}`,
-              Variables: { ...data },
-              TemplateErrorReporting: {
-                Email: "oludavidconnect@gmail.com",
-                name: "Olubisi David",
+                To: [
+                  {
+                    Email: email,
+                    Name: email,
+                  },
+                ],
+                TemplateID: process.env.MAILJET_TEMPLATE_ID,
+                TemplateLanguage: true,
+                Subject: `Details about ${data.Title}`,
+                Variables: { ...data },
+                TemplateErrorReporting: {
+                  Email: "oludavidconnect@gmail.com",
+                  name: "Olubisi David",
+                },
               },
-            },
-          ],
-        })
-        .then(() => {
-          res.status(200).json({ status: "message sent" });
-        })
-        .catch((error) => {
-          res.status(400).json({ status: "message error", error });
-        });
+            ],
+          })
+          .then(() => {
+            res.status(200).json({ status: "message sent" });
+          });
+      } catch (error) {
+        res.status(400).json({ status: "message error", error });
+      }
     }
   }
 }
